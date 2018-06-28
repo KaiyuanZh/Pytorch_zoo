0034eb414fcc27691fb038c85b1cffb8657ca08d
Ky-zhang <zhangkaiyuan20@gmail.com>
2018-06-28 17:43:24 +0800

    data loader

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -20,9 +20,11 @@ class DiabetesDataset(Dataset):
         self.x_data = torch.from_numpy(xy[:, 0:-1])
         self.y_data = torch.from_numpy(xy[:, [-1]])
 
+    # return one item on the index
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
     
+    # return the data length
     def __len__(self):
         return self.len
     
